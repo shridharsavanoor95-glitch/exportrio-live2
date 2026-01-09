366afc13a802ee5eb82e96ff145879c821dd8d21
shridharsavanoor95-glitch <251548979+shridharsavanoor95-glitch@users.noreply.github.com>
2026-01-09 08:39:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Exportrio</h1>
+      <p>Premium Indian Rice Exporter</p>
+      <p>FOB India · 25 MT / 20ft</p>
+      <p>Ports: Nhava Sheva · Mundra · Kandla</p>
+    </main>
+  );
+}
+
