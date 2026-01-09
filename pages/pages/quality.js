dd6d8436046d93ef7bc4991090848c07292215cd
shridharsavanoor95-glitch <shridharsavanoor95@gmail.com>
2026-01-09 17:54:30 +0530

    Create quality.js

---
+++ pages/pages/quality.js
@@ -0,0 +1,18 @@
+export default function Quality() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Quality & Certifications</h1>
+      <ul>
+        <li>APEDA Registered Exporter</li>
+        <li>FSSAI Compliant</li>
+        <li>ISO / HACCP Certified Mills</li>
+        <li>Phytosanitary & Fumigation Certificates</li>
+        <li>SGS / Intertek Inspection on request</li>
+      </ul>
+
+      <p style={{ marginTop: 20 }}>
+        FOB India · 25 MT / 20ft · Ports: Nhava Sheva, Mundra, Kandla
+      </p>
+    </main>
+  );
+}
