7398283e2261b5e5e9378cb0443a6d5767957714
shridharsavanoor95-glitch <shridharsavanoor95@gmail.com>
2026-01-09 17:09:03 +0530

    Create page.js

---
+++ app/markets/page.js
@@ -0,0 +1,9 @@
+export default function MarketsPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Export Markets</h1>
+      <p>Africa – Benin, Senegal, Kenya</p>
+      <p>FOB India · 25 MT / 20ft</p>
+    </main>
+  );
+}
